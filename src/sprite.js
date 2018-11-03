818a4884a84fcc04291063e03c7655f056440794
R.F.D <3382114+RFD@users.noreply.github.com>
2018-11-04 05:12:51 +0900

    Update: マルチ画像のスプライトで表示インデックスをセットできるように

--- src/sprite.js
+++ src/sprite.js
@@ -4,23 +4,10 @@
 class Sprite{
 
 	constructor(img){
-		this.entities	= [];
-		let rect		= null;
-
-		if(img.width>0 && img.height>0 && img.nSplitX>0 && img.nSplitY>0){
-			const widthPerPiece		= parseInt( img.width /img.nSplitX );
-			const heightPerPiece	= parseInt( img.height/img.nSplitY );
-
-			for(let i=0;i<img.nSplitX;++i){
-				for(let j=0;j<img.nSplitY;++j){
-					rect	= cc.rect( i*widthPerPiece, j*heightPerPiece, widthPerPiece, heightPerPiece	);
-					this.entities.push(new cc.Sprite(img.path,rect));
-				}
-			}
-		}
-		else{
-			this.entities[0]	= new cc.Sprite(img.path);
-		}
+		this.img			= img;
+		this.currentIndex	= -1;
+		this.entity			= new cc.Sprite(img.path);
+		this.setIndex(0);
 	}
 
 	/** インスタンス生成
@@ -34,44 +21,64 @@ class Sprite{
 	}
 
 	/** インスタンス取得
-	 * @param {number} [idx=0]
 	 * @returns
 	 * @memberof Sprite
 	 */
-	GetInstance(idx=0){
-		return this.entities[idx];
+	GetInstance(){
+		return this.entity;
 	}
 
 	/** cc.Sprite.attrのラッパ
-	 * @param {Number} idx
 	 * @param {*} attr
 	 * @returns this
 	 * @memberof Sprite
 	 */
-	Attr(idx,attr){
-		this.entities[idx].attr(attr);
+	Attr(attr){
+		this.entity.attr(attr);
 		return this;
 	}
 
 	/** レイヤに自身を追加
-	 * @param {Number} idx
 	 * @param {*} layer
 	 * @returns this
 	 * @memberof Sprite
 	 */
-	AddToLayer(idx,layer){
-		layer.addChild(this.entities[idx]);
+	AddToLayer(layer){
+		layer.addChild(this.entity);
 		return this;
 	}
 
 	/** イベントリスナを追加
-	 * @param {Number} idx
 	 * @param {*} listener
 	 * @returns this
 	 * @memberof Sprite
 	 */
-	AddEventListener(idx,listener){
-		cc.eventManager.addListener(listener,this.entities[idx]);
+	AddEventListener(listener){
+		cc.eventManager.addListener(listener,this.entity);
+		return this;
+	}
+
+	/** マルチ画像スプライトで表示インデックスをセットする
+	 * @param {*} idx
+	 * @returns this
+	 * @memberof Sprite
+	 */
+	setIndex(idx){
+		if(this.currentIndex===idx)	return this;
+
+		if(this.img.width>0 && this.img.height>0 && this.img.nSplitX>0 && this.img.nSplitY>0){
+			console.log("Update Texture Rect");
+			const x	= parseInt( idx % this.img.nSplitX );
+			const y	= parseInt( idx / this.img.nSplitX );
+			const w	= parseInt( this.img.width /this.img.nSplitX );
+			const h	= parseInt( this.img.height/this.img.nSplitY );
+
+			this.entity.setTextureRect(cc.rect( x*w, y*h, w, h)	);
+			this.currentIndex	= idx;
+		}
+		else{
+			this.currentIndex	= 0;
+		}
 		return this;
 	}
 }
