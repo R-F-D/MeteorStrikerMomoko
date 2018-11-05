4c57759cdd644da8f32ff01d57a76bafc72071eb
R.F.D <3382114+RFD@users.noreply.github.com>
2018-11-05 19:35:41 +0900

    Update: indxOf()のラッパ関数IsAnyOf()を追加

--- src/app.js
+++ src/app.js
@@ -6,3 +6,13 @@ Scenes.SceneFactory	= ()=>{
 	const sceneClass	= Scenes.GamePlay;
 	return sceneClass.Create();
 }
+
+/** 配列のいずれかの要素に該当するなら真
+ * @param {*} one
+ * @param {Array} list
+ * return boolean
+ */
+function IsAnyOf(one,list){
+	return list.indexOf(one) >= 0;
+}
+
