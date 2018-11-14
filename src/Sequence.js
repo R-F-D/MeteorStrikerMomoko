6ad11b39c848be1fec1b0baf60d6f2ac75edd6ff
R.F.D <3382114+RFD@users.noreply.github.com>
2018-11-14 12:22:45 +0900

    Add: Sequenceクラスを追加

---
+++ src/Sequence.js
@@ -0,0 +1,61 @@
+/* *******************************************************************************
+	シークエンスクラス
+********************************************************************************/
+var cc;
+
+
+/** シークエンス実体クラス
+ * @class Sequence
+ */
+class Sequence{
+	constructor(){
+		this.listenTargetLayer	= null;
+		this.eventListeners		= [];
+		this.Init();
+	}
+
+	/** インスタンス生成
+	 * @static
+	 * @returns this
+	 * @memberof Sequence
+	 */
+	static Create(){return new this();}
+
+	/** 初期化
+	 * @returns this
+	 * @memberof Sequence
+	 */
+	Init(){
+		//イベントリスナ
+		if(this.listenTargetLayer){
+			cc.eventManager.removeListeners(this.listenTargetLayer);
+			for(let i in this.eventListeners){
+				cc.eventManager.addListener(this.eventListeners[i],this.listenTargetLayer);
+			}
+		}
+		return this;
+	}
+
+	/** イベントリスナの対象レイヤを設定
+	 * @param {*} layer
+	 * @returns
+	 * @static
+	 * @memberof Sequence
+	 */
+	SetListenerTarget(layer){
+		Scenes.SetListenerTarget(layer);
+		return this;
+	}
+
+	/** イベントリスナを設定
+	 * @param {*} listeners
+	 * @returns
+	 * @memberof Sequence
+	 */
+	SetEventListeners(listeners){
+		if(typeof listeners==='array')	this.eventListeners	= listeners;
+		else							this.eventListeners	= [listeners];
+		return this;
+	}
+}
+
