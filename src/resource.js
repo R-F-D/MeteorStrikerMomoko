0f1aaa4eb754b687c0a38a99b28819f603b948f1
R.F.D <3382114+RFD@users.noreply.github.com>
2018-11-21 05:06:12 +0900

    Update: リソースファイルのディレクトリを指定できるよう変更

--- src/resource.js
+++ src/resource.js
@@ -1,15 +1,22 @@
+/** @const リソースファイル設定 */
 const rc	= {
-	"img"	: {
-		"player"	: [ "player.png",	2,	1,	],
-		"aimCursor"	: [ "aimcur.png",	1,	1,	],
-		"aimGauge"	: [ "aimgauge.png",	1,	1,	],
+	/** リソースディレクトリ */
+	DIRECTORY	: "../res/",
+
+	/** 画像 */
+	img	: {
+		"player"	: [ "player.png",	2,	1,	],	//プレイヤーキャラクター
+		"aimCursor"	: [ "aimcur.png",	1,	1,	],	//エイミングカーソル
+		"aimGauge"	: [ "aimgauge.png",	1,	1,	],	//エイミングゲージ
 	},
-	"se"	: {
+
+	/** サウンド */
+	se	: {
 	},
 };
 
 
 var g_resources = [];
 for(let i in rc.img) {
-	g_resources.push("../res/"+rc.img[i][0]);
+	g_resources.push(`${rc.DIRECTORY}${rc.img[i][0]}`);
 }
