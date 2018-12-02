460576cdacd2c2242b86f774c45359816c6ac8b9
R.F.D <3382114+RFD@users.noreply.github.com>
2018-12-02 20:04:15 +0900

    Update: Cycle()を追加

--- src/app.js
+++ src/app.js
@@ -8,6 +8,33 @@ Scenes.SceneFactory	= ()=>{
 }
 
 
+/** 数値を一定範囲内に収める（サイクル）
+ * @param {number|number[]} value
+ * @param {number} lower 下限値（以上）
+ * @param {number} upper 上限値（未満）
+ * @returns  {number|number[]}
+ */
+function Cycle(value,lower,upper){
+	if(typeof lower!=="number" || typeof upper!=="number" || lower>=upper){
+		throw new Error(`'${lower}' or '${upper}' is not valid.`);
+	}
+	//To Array
+	const isArray	= Array.isArray(value);
+	if(!isArray)	value	= [value];
+	//サイクル処理
+	value	= value.map((v)=>{
+		if(typeof v !== "number")	throw new Error(`'${v}' is not a number.`);
+
+		while(true){
+			if     (v < lower)	v += upper-lower;
+			else if(upper <= v)	v -= upper-lower;
+			else				return v;
+		}
+	});
+	return isArray	? value	: value[0];
+}
+
+
 //デバッグ
 function isDebug(){			return !!cc.game.config[cc.game.CONFIG_KEY.debugMode];	}
 function Debug(callback){	if(isDebug()) callback();	}
