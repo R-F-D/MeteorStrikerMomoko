6497b082ba2c85fe7344106f0309b823f3332527
R.F.D <3382114+RFD@users.noreply.github.com>
2018-12-05 06:48:12 +0900

    Update: Clamp()とCycle()の配列対応をプロトタイプに分離

--- src/app.js
+++ src/app.js
@@ -7,51 +7,58 @@ Scenes.SceneFactory	= ()=>{
 	return sceneClass.Create();
 }
 
+
 /** 数値を一定範囲内に収める（サイクル）
- * @param {number|number[]} value
+ * @param {number} value 対象の値
  * @param {number} lower 下限値（以上）
  * @param {number} upper 上限値（未満）
- * @returns  {number|number[]}
+ * @returns  {number}
  */
 function Cycle(value,lower,upper){
-	if(typeof lower!=="number" || typeof upper!=="number" || lower>=upper){
-		throw new Error(`'${lower}' or '${upper}' is not valid.`);
+	if(typeof value !== "number" || typeof lower!=="number" || typeof upper!=="number" || lower>=upper){
+		throw new Error(`'${value}' or ${lower}','${upper}' is not valid.`);
 	}
-	//To Array
-	const isArray	= Array.isArray(value);
-	if(!isArray)	value	= [value];
-	//サイクル処理
-	value	= value.map((v)=>{
-		if(typeof v !== "number")	throw new Error(`'${v}' is not a number.`);
 
-		while(true){
-			if     (v < lower)	v += upper-lower;
-			else if(upper <= v)	v -= upper-lower;
-			else				return v;
-		}
-	});
-	return isArray	? value	: value[0];
+	while(true){
+		if     (value < lower)	value += upper-lower;
+		else if(upper <= value)	value -= upper-lower;
+		else					return value;
+	}
+}
+
+/** 配列の全ての数値を一定範囲内に収める（サイクル）
+ * @param {number} value 対象の値
+ * @param {number} lower 下限値（以上）
+ * @param {number} upper 上限値（未満）
+ * @returns {number[]}
+ */
+Array.prototype.Cycle	= function(lower,upper){
+	return this.map(v=>Cycle(v,lower,upper));
 }
 
 /** 数値を一定範囲内に収める（クランプ）
- * @param {number} value
+ * @param {number} value 対象の値
  * @param {number} [lower=null] 下限値（以上）
  * @param {number} [upper=null] 上限値（以下）
- * @returns
+ * @returns {number} 新しい値
  */
 function Clamp(value,lower=null,upper=null){
-	if(lower!=null && upper!=null && upper<lower) upper = [lower,lower=upper][0];	//Swap
-
-	//To Array
-	const isArray	= Array.isArray(value);
-	if(!isArray)	value	= [value];
+	//Swap
+	if(lower!=null && upper!=null && upper<lower) [upper,lower] = [lower,upper];
 	//クランプ処理
-	value	= value.map((v)=>{
-		if     (lower!=null && v<lower)	return lower;
-		else if(upper!=null && v>upper)	return upper;
-		return v;
-	});
-	return isArray	? value	: value[0];
+	if     (lower!=null && value<lower)	return lower;
+	else if(upper!=null && value>upper)	return upper;
+	return value;
+}
+
+/** 配列の全ての数値を一定範囲内に収める（クランプ）
+ * @param {number[]} value 対象の値
+ * @param {number} [lower=null] 下限値（以上）
+ * @param {number} [upper=null] 上限値（以下）
+ * @returns {number[]} 新しい値
+ */
+Array.prototype.Clamp	= function(lower=null,upper=null){
+	return this.map(v=>Clamp(v,lower,upper));
 }
 
 /** 配列作成
