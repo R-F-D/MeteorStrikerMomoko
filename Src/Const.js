ff074b73088acbbf9759090b72195e5f1e15c553
R.F.D <3382114+RFD@users.noreply.github.com>
2019-05-28 10:14:46 +0900

    Clean: 定数ファイル内では値を変更できるよう記述を変更

--- Src/Const.js
+++ Src/Const.js
@@ -1,16 +1,16 @@
 /********************************************************************************
 	定数
 ********************************************************************************/
-const C = {
+const C = (()=>{let C={}; //file scope
 
 /** @const ローカルストレージのキー */
-Store	: {
+C.Store	= {
 	/** @const ハイスコア*/
 	HighScore:	"HighScore",
 },
 
 /** @const チェックポイント距離 */
-CheckPoints	: [
+C.CheckPoints	= [
 	{	key:"Start",	distance:0,			},
 	{	key:"Venus",	distance:42000000,	},
 	{	key:"Mars",		distance:42000000,	},
@@ -18,8 +18,9 @@ CheckPoints	: [
 	{	key:"Sun",		distance:149600000,	},
 	{	key:"Kirari",	distance:186200000,	},
 	{	key:"Unicorn",	distance:256000000,	},
-],
+];
 
 
-//Const
-};
+return C})(); //file scope
+
+
