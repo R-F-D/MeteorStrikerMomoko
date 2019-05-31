1bfb03764a75463c9d2e066799e4b610681aaa2a
R.F.D <3382114+RFD@users.noreply.github.com>
2019-05-31 23:43:28 +0900

    Fix: チェックポイントの距離のミスを修正

--- Src/Const.js
+++ Src/Const.js
@@ -13,7 +13,7 @@ C.Store	= {
 C.CheckPoints	= [
 	{	key:"Start",	distance:0,			},
 	{	key:"Venus",	distance:42000000,	},
-	{	key:"Mars",		distance:42000000,	},
+	{	key:"Mars",		distance:70000000,	},
 	{	key:"Mercury",	distance:91500000,	},
 	{	key:"Sun",		distance:149600000,	},
 	{	key:"Kirari",	distance:186200000,	},
