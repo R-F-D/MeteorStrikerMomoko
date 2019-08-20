cdf7408600e54186b9d48b1cd27272b110ffd379
R.F.D <3382114+RFD@users.noreply.github.com>
2019-08-20 15:06:34 +0900

    Clean: Soundにコメント追加

--- Src/Sound.js
+++ Src/Sound.js
@@ -12,9 +12,13 @@ class Sound{
 		this.musicVolume	= 1.0;
 	}
 
+	/** 初期化処理
+	 * - 自動再生ブロックへの対応のため音量0で再生
+	 * - Init()前後に画面をクリック/タップさせるような構成にすること */
 	Init(){
 		if(this.musicIsInitialized)	return this;
 
+		//音量0でBGMを再生してみる
 		cc.audioEngine.setMusicVolume(0);
 		cc.audioEngine.playMusic(`${rc.DIRECTORY}Bgm/${rc.bgm.meteorite}`,false);
 		cc.audioEngine.stopMusic();
@@ -24,11 +28,13 @@ class Sound{
 		return this;
 	}
 
+	/** BGM再生 */
 	PlayMusic(path){
 		cc.audioEngine.playMusic(`${rc.DIRECTORY}Bgm/${path}`,true);
 		return this;
 	}
 
+	/** BGM停止 */
 	StopMusic(){
 		cc.audioEngine.stopMusic();
 		return this;
