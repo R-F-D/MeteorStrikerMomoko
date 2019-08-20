12fe3f8037975eeccd3c0cd3cca55a35b4d98e63
R.F.D <3382114+RFD@users.noreply.github.com>
2019-08-21 06:25:47 +0900

    Add: Logo.js追加

---
+++ Src/Scene/Logo.js
@@ -0,0 +1,95 @@
+/* *******************************************************************************
+	Logoシーン
+********************************************************************************/
+var Scene	= Scene || {};
+(function(){	//File Scope
+
+/** リンクされたレイヤーのタグ */
+const LinkedLayerTags	= {
+	MAIN	: "Logo.Main",
+};
+
+
+Scene.Logo	= class extends Scene.SceneBase {
+
+	constructor(){
+		super(true);
+
+		this.Sequences	= {
+			INITIAL:	null,	//初期状態
+		};
+
+		this.label			= null;
+
+		/** ccSceneのインスタンス */
+		this.ApplicateCcSceneInstance(this).InitLayerList();
+
+		//シークエンス設定
+		for(let i in this.Sequences){ this.Sequences[i] = Scene.Sequence.Create() }
+		this.SetSequenceFunctions().InitEventListenerList();
+	}
+
+
+	/** ccLayerに渡す用 */
+	InitLayerList(){
+		const _this	= this;
+		super.InitLayerList()
+			.AddToLayerList("main",{
+				ctor:function(){
+					this._super();
+					this.scheduleUpdate();
+					_this.label	= Label.CreateInstance(14).AddToLayer(this);
+					return true;
+				},
+			})
+		return this;
+	}
+
+	OnEnter(){
+		super.OnEnter();
+		this.SetLayer(LinkedLayerTags.MAIN,this.ccLayers.main,0x0001);	//各種処理があるのでmainレイヤは最後にセット
+
+		this.InitSequences(this.Sequences,LinkedLayerTags.MAIN,this.ccLayerInstances[LinkedLayerTags.MAIN])
+			.SetSequence(this.Sequences.INITIAL);
+
+		return this;
+	}
+
+	OnUpdating(dt){
+		super.OnUpdating(dt);
+		return this;
+	}
+
+	SetSequenceFunctions(){
+		const size		= cc.director.getWinSize();
+
+		//初期状態
+		this.Sequences.INITIAL.PushStartingFunctions(()=>{
+			this.label
+				.SetAnchorPoint(cc.p(1,0))
+				.SetPosition(size.width-8,4)
+				.SetFontColor("#4F4F4F")
+				.SetString(L.Text("Logo.Wait"));
+		})
+		.PushUpdatingFunctions(dt=>{
+		});
+
+		return this;
+	}
+
+	InitEventListenerList(){
+		super.InitEventListenerList();
+
+		//共通イベント対応設定
+		let commonEvents	= [];
+		commonEvents.push(this.listeners.touched);
+		commonEvents.push(this.listeners.keyboardReset);
+		this.SetCommonEventListeners("SceneBase.TouchFx",commonEvents);
+
+		return this;
+	}
+
+}//class
+
+})();	//File Scope
+
