aca80d1751bc269e78c7fce62ccbaaacd07cc97d
R.F.D <3382114+RFD@users.noreply.github.com>
2019-09-17 23:09:21 +0900

    Update: Records表示板の座標調整

--- Src/Scene/Records.js
+++ Src/Scene/Records.js
@@ -14,11 +14,11 @@ const PanelPosition		= {X:16+64,Y:16};
 const RecordBoard	= {
 	MaxRows:			5,
 	MaxColumns:			2,
-	Size:				{Width:32*6-1, Height:32-1},
+	Size:				{Width:32*5, Height:32-1},
 	NumLogLines:		2,
 };
 const AchievementBoard	= {
-	MaxRows:			4,
+	MaxRows:			3,
 	MaxColumns:			1,
 	Size:				{Width:32*11-64, Height:64-1},
 	NumLogLines:		4,
@@ -180,7 +180,7 @@ Scene.Records	= class extends Scene.SceneBase {
 						text			= L.TextExists(`Records.${handle.Key}.Secret`) ? L.Text(`Records.${handle.Key}.Secret`) : L.Text("Records.Secret");
 					}
 
-					const x	= Math.trunc(i/RecordBoard.MaxRows) * (RecordBoard.Size.Width+1);
+					const x	= Math.trunc(i/RecordBoard.MaxRows) * (RecordBoard.Size.Width+32);
 					const y	= (i%RecordBoard.MaxRows) * (RecordBoard.Size.Height+1);
 					board.body.bg.lower			= {width:RecordBoard.Size.Width, height:RecordBoard.Size.Height};
 					board.body.bg.animationDelay= 0.05*i;
