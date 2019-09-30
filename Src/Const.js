1815763f9d249911559501e4c3dc891a6502a4cd
R.F.D <3382114+RFD@users.noreply.github.com>
2019-09-30 15:05:21 +0900

    Fix: Const.jsの定数記述を変更

--- Src/Const.js
+++ Src/Const.js
@@ -1,22 +1,22 @@
 /********************************************************************************
 	定数
 ********************************************************************************/
-const C	= {};
+var C	= (()=>{
+const C={};
+
+	/** @const チェックポイント距離 */
+	C.Check	= [
+		{	key:"Start",	distance:0,			storage:null,	},
+		{	key:"Venus",	distance:42000000,	storage:null,	},
+		{	key:"Mars",		distance:70000000,	storage:null,	},
+		{	key:"Mercury",	distance:91500000,	storage:null,	},
+		{	key:"Sun",		distance:149600000,	storage:0,		},
+		{	key:"Kirari",	distance:186200000,	storage:1,		},
+		{	key:"Unicorn",	distance:256000000,	storage:2,		},
+	];
+
+	/** @const ウェブサイトURL */
+	C.WebPage	= "https://example.jp";
 
 
-/** @const チェックポイント距離 */
-C.Check	= [
-	{	key:"Start",	distance:0,			storage:null,	},
-	{	key:"Venus",	distance:42000000,	storage:null,	},
-	{	key:"Mars",		distance:70000000,	storage:null,	},
-	{	key:"Mercury",	distance:91500000,	storage:null,	},
-	{	key:"Sun",		distance:149600000,	storage:0,		},
-	{	key:"Kirari",	distance:186200000,	storage:1,		},
-	{	key:"Unicorn",	distance:256000000,	storage:2,		},
-];
-
-/** @const ウェブサイトURL */
-C.WebPage	= "https://example.jp";
-
-
-
+return C;})();
